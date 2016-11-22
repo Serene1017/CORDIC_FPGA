0a4a0025d08c1b0cba6eb3f614b352a609f0a18d
Serene1017 <wangtaoran040208@gmail.com>
2016-11-22 16:26:04 -0600

    Update z.v

--- z.v
+++ z.v
@@ -21,33 +21,93 @@
 
 
 module z(
-	input [15:0] z0,
-	input i,
+	output reg sign,	// the sign bit of zreg
+	output reg zdone,	// signal for completing sh&comp
+	input op,	// control signal of the adder/subtracter
+	input [15:0] z0,	// initial value
+	input [4:0] i,	// how many times of pressing button
+	input start,	// start sh&comp
 	input clk
 	);
 	
-	parameter regwidth=16;	
-	parameter romwidth=16;
-	parameter romdepth=16;
+	reg [15:0] zreg;
+	reg [15:0] zrom [0:15];
+	reg [4:0] counter;
+	reg cin_reg;
+	reg zin;
 	
-	reg [regwidth-1:0] zreg;
-	reg [romwidth-1:0] zrom [0:romdepth-1];
-	reg [3:0] counter;
+	wire zn;
+	wire cout;
+	wire a,b,cin;
 	
-	//assign zin=zrom[counter];
-	fa1bit adder(cout,zn,zin,zreg[0],cin);
+	initial
+	begin
+		zrom[0]=16'b0011_0010_0100_0011;
+		zrom[1]=16'b0001_1101_1010_1100;
+		zrom[2]=16'b0000_1111_1010_1101;
+		zrom[3]=16'b0000_0111_1111_0101;
+		zrom[4]=16'b0000_0011_1111_1110;
+		zrom[5]=16'b0000_0001_1111_1111;
+		zrom[6]=16'b0000_0000_1111_1111;
+		zrom[7]=16'b0000_0000_0111_1111;
+		zrom[8]=16'b0000_0000_0011_1111;
+		zrom[9]=16'b0000_0000_0001_1111;
+		zrom[10]=16'b0000_0000_0000_1111;
+		zrom[11]=16'b0000_0000_0000_0111;
+		zrom[12]=16'b0000_0000_0000_0011;
+		zrom[13]=16'b0000_0000_0000_0001;
+		zrom[14]=16'b0;
+		zrom[15]=16'b0;
+	end
 	
+	fa1bit adder(cout,zn,a,b,cin,op); // inputs should be wire type
+	
+	
+	// shift register
 	always @(posedge clk)
 	begin
-		if(i=0)
+		if(i==5'b0)
+		/* 
+			initial status
+		*/
+		begin
 			zreg=z0;
+			cin_reg=0;
+			sign=zreg[15];
+			zin=zrom[0][0];
+			zdone=0;
 			counter=0;
+		end
 		else
 		begin
-			zreg<={zn,zreg[15:1]};
-			zin=zrom[counter][0];
-			counter=counter+1;
+			if(start)
+			begin
+				if(counter==5'b1_0000)	// finish shifting 16 times
+				begin
+					zreg=zreg;
+					zdone=1;
+					sign=zreg[15];
+				end
+				else
+				begin
+					zreg<={zn,zreg[15:1]};
+					cin_reg<=cout;
+					counter<=counter+1;
+					zin<=zrom[i-1][counter];
+				end
+			end
+			else // if not started the sh&comp
+			begin
+				zreg<=zreg;
+				counter=0;
+				zdone=0;
+			end
 		end
 	end
 	
+	// convert reg type to wire type for 1bit adder/subtracter 
+	assign a=zreg[0];
+	assign b=zin;
+	assign cin=cin_reg;
+
 endmodule
